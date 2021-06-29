1fc481cb6a2ffdb64079e555201efb63dc7af747
Ankit Raghuram <ankit.raghuram.36@gmail.com>
2021-06-29 12:07:20 -0500

    added database

--- first_python_file.py
+++ first_python_file.py
@@ -1,2 +1,22 @@
 # my first python file
 print('Hello World')
+
+# import datasets
+from sklearn import datasets
+print('Loaded datasets')
+
+# load our breastcancer dataset
+bc_dataset = datasets.load_breast_cancer()
+
+# bunch of print statements to visualize the code
+# print(bc_dataset.keys())
+# # data is input features & target is the labels for supervised learning
+
+# print(bc_dataset.feature_names)
+# print(bc_dataset.target_names)
+# print(bc_dataset.DESCR)
+# print(bc_dataset.filename)
+
+# look a size of dataset
+print(len(bc_dataset.data))
+print(bc_dataset.data[500])
